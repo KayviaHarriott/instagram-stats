45ccc2df53e36ccbe2d0e532ee813d39543290d2
Kayvia Harriott <kayviaharriott@hotmail.com>
2025-04-26 08:18:21 -0500

    Started addition of google analytics tag using fake tag

--- instagram-stats/vite.config.ts
+++ instagram-stats/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import {VitePluginRadar} from 'vite-plugin-radar'
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    VitePluginRadar({
+      analytics: {
+        id: 'G-1234567890',
+      }
+    })
   ],
 })
