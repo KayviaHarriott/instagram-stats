8372c6725543524a36b1c8c71942bdcf14960c0f
Kayvia Harriott <kayviaharriott@hotmail.com>
2025-04-26 08:31:18 -0500

    Modified vite config to use plugin for env file

--- instagram-stats/vite.config.ts
+++ instagram-stats/vite.config.ts
@@ -1,14 +1,19 @@
-import { defineConfig } from 'vite'
+
+import { defineConfig, loadEnv } from 'vite';
 import tailwindcss from '@tailwindcss/vite'
 import {VitePluginRadar} from 'vite-plugin-radar'
 
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    VitePluginRadar({
-      analytics: {
-        id: 'G-1234567890',
-      }
-    })
-  ],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const GOOGLE_TAG = `${env.VITE_GTAG_ID}`;
+
+  return {
+    plugins: [tailwindcss(),
+      VitePluginRadar({
+        analytics: {
+          id: `G-${GOOGLE_TAG}`,
+        }
+      })],
+  };
+});
