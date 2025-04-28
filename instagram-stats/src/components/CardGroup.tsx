4d5180bd8d7cbc62aff05764549337437d0b6c32
Kayvia Harriott <kayviaharriott@hotmail.com>
2025-04-27 21:41:55 -0500

    Removed unused text

--- instagram-stats/src/components/CardGroup.tsx
+++ instagram-stats/src/components/CardGroup.tsx
@@ -1,40 +1,40 @@
 import { MiniUserCards } from "./MiniUserCards";
 
 interface CardGroupProps {
-   children: { value: string;
-    timestamp: number;
-    href: string}[];
+  children: { value: string; timestamp: number; href: string }[];
 }
 
 export const CardGroup: React.FC<CardGroupProps> = ({ children }) => {
-return (
+  return (
     <div>
-        <p>Hello</p>
-    <div className="flex flex-wrap gap-1">
-      {(children ?? [])
-        .reduce<
-          { value: string; timestamp: number; href: string }[][]
-        >((chunks, req, index) => {
-          const chunkIndex = Math.floor(index / 3);
-          if (!chunks[chunkIndex]) {
-            chunks[chunkIndex] = [];
-          }
-          chunks[chunkIndex].push(req);
-          return chunks;
-        }, [])
-        .map((chunk, chunkIndex) => (
-          <div className="flex gap-2 w-full" key={chunkIndex}>
-            {chunk.map((req, index) => (
-                <MiniUserCards key={index}
-                username={`${req.value}`}
-                description={`${new Date(
-                  req.timestamp * 1000
-                ).toLocaleString()}`}
-                link={req.href} />
-            ))}
-          </div>
-        ))}
+      <div className="flex flex-wrap gap-1">
+        {(children ?? [])
+          .reduce<{ value: string; timestamp: number; href: string }[][]>(
+            (chunks, req, index) => {
+              const chunkIndex = Math.floor(index / 3);
+              if (!chunks[chunkIndex]) {
+                chunks[chunkIndex] = [];
+              }
+              chunks[chunkIndex].push(req);
+              return chunks;
+            },
+            []
+          )
+          .map((chunk, chunkIndex) => (
+            <div className="flex gap-2 w-full" key={chunkIndex}>
+              {chunk.map((req, index) => (
+                <MiniUserCards
+                  key={index}
+                  username={`${req.value}`}
+                  description={`${new Date(
+                    req.timestamp * 1000
+                  ).toLocaleString()}`}
+                  link={req.href}
+                />
+              ))}
+            </div>
+          ))}
+      </div>
     </div>
- </div>
-)
-}
+  );
+};
